8e7e0b83764ea7f92ebb14eb9fdb1f31a25258b8
Esin <nbusra.esin@gmail.com>
2023-03-31 14:07:14 +0300

    done

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(x){
+  // Write your code here
+  if (x <= 400){
+    return "This one is on me!"
+  } else if (x > 400 && x <= 2000){
+    return "That will be twenty bucks."
+  } else if (x > 2000  && x < 2500){
+    return "I will gladly take your thirty bucks."
+  } else{
+    return "No can do."
+  }
+  
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(x){
   // Write your code here!
+  if (x == "NYC"){
+    return "Ok, sounds good."
+  } else {
+    return "No go."
+  } 
 }
-
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(x){
   // Write your code here!
+  if (x == "generous"){
+    return "Thank you so much."
+  } else if (x == "not as generous"){
+    return "Thank you."
+  } else {
+    return "Bye."
+  }
 }
